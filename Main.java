5e505a727784cad49d5f3f2dac2d994df0893612
Adelssaya <lorenzonova62@gmail.com>
2025-06-02 11:46:54 -0600

    cambios

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(string[] args) {
+        Suma suma = new Suma()
+        Suma.suma(a: 8,b: 14)
+    }
+}
---
+++ suma.java
@@ -0,0 +1,6 @@
+public class Suma {
+    void suma(int a, int b) {
+        int suma = a + b;
+        System.out.println("el resultado de la operacion es:  ");
+    }
+}
